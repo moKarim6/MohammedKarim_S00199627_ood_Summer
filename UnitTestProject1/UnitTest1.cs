499709ce00e0eeee5ffa763c35f1faf9d7e41034
moKarim6 <60603207+moKarim6@users.noreply.github.com>
2021-05-11 10:09:05 +0100

    third commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MohammedKarim_S00199627;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPrice()
+        {
+            // ARRANGE
+            Game g1 = new Game("Mario", 85, "Arcade", "Nintendo", 50m);
+            decimal finalprice = 0m;
+
+            // ACT
+            g1.DecreasePrice(50m);
+
+            // ASSERT
+            Assert.AreEqual(finalprice, g1.Price);
+        }
+    }
+}
